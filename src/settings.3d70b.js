642823a677dc2d1c1b821e5280d7127714f55f88
haiyejunjietang <2545373178@qq.com>
2025-08-27 09:54:43 +0800

    Initial commit of my Cocos game

---
+++ src/settings.3d70b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/start.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"ef0a5",main:"4ef58"}};
